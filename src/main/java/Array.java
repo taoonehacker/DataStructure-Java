dd55f65119e5f055bd595078f4d3708b9a8d70d7
taoonehacker@gmail.com <taoonehacker@gmail.com>
2019-10-09 08:21:06 +0800

    Add add method to Array and ArrayTest

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -1,3 +1,5 @@
+import java.security.PublicKey;
+
 public class Array {
   private int[] data;
   private int size;
@@ -21,5 +23,25 @@ public class Array {
   public boolean isEmpty(){
     return  this.size==0;
   }
+
+  public void addLast(int e){
+    add(size,e);
+  }
+
+  public void addFirst(int e){
+    add(0,e);
+  }
+
+  public void add(int index,int e){
+    if(size==data.length)
+      throw new IllegalArgumentException("Add failed. Array is full.");
+    if(index<0||index>size)
+      throw new IllegalArgumentException("Add failed. Require index >=0 and index <= size.");
+    for (int i = size-1; i >=index; i--)
+      data[i+1]=data[i];
+    data[index]=e;
+    size++;
+  }
+
 
 }
--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -1,9 +1,15 @@
 import org.junit.*;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class ArrayTest {
+
     private Array array;
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
     @Before
     public void setUp(){
         array = new Array(15);
@@ -21,7 +27,50 @@ public class ArrayTest {
 
     @Test
     public void isEmptyTest(){
+        array = new Array();
         assertTrue(array.isEmpty());
     }
+    @Test
+    public void addTest(){
+        try {
+            array = new Array(2);
+            array.add(0,1);
+            array.add(1,2);
+            array.add(2,3);
+        }catch (IllegalArgumentException ex){
+            assertEquals("Add failed. Array is full.",ex.getMessage());
+        }
+
+        try {
+            array = new Array(15);
+            array.add(16,16);
+        }catch (IllegalArgumentException ex){
+            assertEquals("Add failed. Require index >=0 and index <= size.",ex.getMessage());
+        }
+
+        array = new Array();
+        array.add(0,1);
+        array.add(1,2);
+        array.add(2,3);
+        array.add(3,4);
+        array.add(4,5);
+
+        assertEquals(5,array.getSize());
+        assertEquals(10,array.getCapacity());
+    }
+
+    @Test
+    public void addLastTest(){
+        array.addLast(2);
+        assertEquals(1,array.getSize());
+    }
+    @Test
+    public void addFirstTest(){
+        array.addFirst(1);
+        assertEquals(1,array.getSize());
+    }
+
+
+
 
 }
